75f66284a05ea3f7c0ae1bf32f0d9dd436f4d61a
Melissa Ng <32788311+MelissaN@users.noreply.github.com>
2018-08-06 11:50:12 -0700

    remove comment (#SOLUTION: CHANGE 'i - 1' TO 'i')

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -20,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i, i_arg)  #SOLUTION: CHANGE 'i - 1' TO 'i'
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
