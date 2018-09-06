c65a462bb41c54557889806dd771e3abc2eedfcc
MelissaN <majajachiu@yahoo.com>
2018-09-06 00:36:42 +0000

    bug: modify instance init so square sides are equal

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 
 class square():
-    
+
     width = 0
     height = 0
 
-    
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        if (self.height != self.width):
+            self.height = self.width
 
     def area_of_my_square(self):
         """ Area of the square """
