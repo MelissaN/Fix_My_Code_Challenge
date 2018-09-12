9ab38fdddf5aee7d867143947ec6b83a224fe65d
MelissaN <majajachiu@yahoo.com>
2018-09-12 06:36:24 +0000

    fix styling: caps, method names, docstrings

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,30 +1,52 @@
 #!/usr/bin/python3
+"""
+Class Square Module
+"""
 
-class square():
+
+class Square():
+    """
+    Attr:
+        width
+        height
+    Methods:
+        __init__(self, *args, **kwargs)
+        area(self)
+        perimeter(self)
+        __str__(self)
+    """
 
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        if (self.height != self.width):
-            self.height = self.width
+        """initialize square instance with width and height"""
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+            if (self.height != self.width):
+                self.height = self.width
+        else:
+            self.height = self.width = 0
 
-    def area_of_my_square(self):
-        """ Area of the square """
+    def area(self):
+        """ Return area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perimeter(self):
+        """ Return perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Return string representation """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.width)
+    print(s.height)
+    print(s.area())
+    print(s.perimeter())
+    print(s.__str__())
